1ef8d8a14c051e555cde0a64ecbb6e68a6a19eeb
nenudietzdenise-pixel <nenudietzdenise@gmail.com>
2026-09-16 15:24:30 -0300

    Update products.js

--- products.js
+++ products.js
@@ -33,5 +33,187 @@ const PRODUCTS = [
     "model": "iPhone 15",
     "price": "Consultar precio",
     "image": "images/iphone15-lila-estrellas.webp"
-  }
+  },
+  {
+  "id": "iphone13-2",
+  "name": "Funda iPhone 13",
+  "model": "iPhone 13",
+  "price": "Consultar precio",
+  "image": "images/iphone 13 (2).jpeg"
+},
+{
+  "id": "iphone13-3",
+  "name": "Funda iPhone 13",
+  "model": "iPhone 13",
+  "price": "Consultar precio",
+  "image": "images/iphone 13 (3).jpeg"
+},
+{
+  "id": "iphone13-4",
+  "name": "Funda iPhone 13",
+  "model": "iPhone 13",
+  "price": "Consultar precio",
+  "image": "images/iphone 13 (4).jpeg"
+},
+{
+  "id": "iphone13-6",
+  "name": "Funda iPhone 13",
+  "model": "iPhone 13",
+  "price": "Consultar precio",
+  "image": "images/iphone 13 (6).jpeg"
+},
+{
+  "id": "iphone13-7",
+  "name": "Funda iPhone 13",
+  "model": "iPhone 13",
+  "price": "Consultar precio",
+  "image": "images/iphone 13 (7).jpeg"
+},
+{
+  "id": "iphone13-pro-1",
+  "name": "Funda iPhone 13 Pro",
+  "model": "iPhone 13 Pro",
+  "price": "Consultar precio",
+  "image": "images/iphone 13 pro.jpeg"
+},
+{
+  "id": "iphone15-3",
+  "name": "Funda iPhone 15",
+  "model": "iPhone 15",
+  "price": "Consultar precio",
+  "image": "images/iphone 15 (3).jpeg"
+},
+{
+  "id": "iphone15-6",
+  "name": "Funda iPhone 15",
+  "model": "iPhone 15",
+  "price": "Consultar precio",
+  "image": "images/iphone 15 (6).jpeg"
+},
+{
+  "id": "iphone15-7",
+  "name": "Funda iPhone 15",
+  "model": "iPhone 15",
+  "price": "Consultar precio",
+  "image": "images/iphone 15 (7).jpeg"
+},
+{
+  "id": "iphone15-8",
+  "name": "Funda iPhone 15",
+  "model": "iPhone 15",
+  "price": "Consultar precio",
+  "image": "images/iphone 15 (8).jpeg"
+},
+{
+  "id": "iphone15-9",
+  "name": "Funda iPhone 15",
+  "model": "iPhone 15",
+  "price": "Consultar precio",
+  "image": "images/iphone 15 (9).jpeg"
+},
+{
+  "id": "iphone15-nueva",
+  "name": "Funda iPhone 15",
+  "model": "iPhone 15",
+  "price": "Consultar precio",
+  "image": "images/iphone 15.png"
+},
+{
+  "id": "iphone16-1",
+  "name": "Funda iPhone 16",
+  "model": "iPhone 16",
+  "price": "Consultar precio",
+  "image": "images/iphone 16.jpeg"
+},
+{
+  "id": "iphone16-promax-1",
+  "name": "Funda iPhone 16 Pro Max",
+  "model": "iPhone 16 Pro Max",
+  "price": "Consultar precio",
+  "image": "images/iphone 16 pro max.jpeg"
+},
+{
+  "id": "iphone16-promax-2",
+  "name": "Funda iPhone 16 Pro Max",
+  "model": "iPhone 16 Pro Max",
+  "price": "Consultar precio",
+  "image": "images/iphone 16 pro max (2).jpeg"
+},
+{
+  "id": "iphone16-promax-3",
+  "name": "Funda iPhone 16 Pro Max",
+  "model": "iPhone 16 Pro Max",
+  "price": "Consultar precio",
+  "image": "images/iphone 16 pro max (3).jpeg"
+},
+{
+  "id": "iphone17-1",
+  "name": "Funda iPhone 17",
+  "model": "iPhone 17",
+  "price": "Consultar precio",
+  "image": "images/iphone 17.jpeg"
+},
+{
+  "id": "iphone17-2",
+  "name": "Funda iPhone 17",
+  "model": "iPhone 17",
+  "price": "Consultar precio",
+  "image": "images/iphone 17 (2).jpeg"
+},
+{
+  "id": "iphone17-pro-1",
+  "name": "Funda iPhone 17 Pro",
+  "model": "iPhone 17 Pro",
+  "price": "Consultar precio",
+  "image": "images/iphone 17 pro.jpeg"
+},
+{
+  "id": "iphone17-pro-2",
+  "name": "Funda iPhone 17 Pro",
+  "model": "iPhone 17 Pro",
+  "price": "Consultar precio",
+  "image": "images/iphone 17 pro (2).jpeg"
+},
+{
+  "id": "iphone17-pro-3",
+  "name": "Funda iPhone 17 Pro",
+  "model": "iPhone 17 Pro",
+  "price": "Consultar precio",
+  "image": "images/iphone 17 pro (3).jpeg"
+},
+{
+  "id": "iphone17-pro-4",
+  "name": "Funda iPhone 17 Pro",
+  "model": "iPhone 17 Pro",
+  "price": "Consultar precio",
+  "image": "images/iphone 17 pro (4).jpeg"
+},
+{
+  "id": "iphone17-pro-5",
+  "name": "Funda iPhone 17 Pro",
+  "model": "iPhone 17 Pro",
+  "price": "Consultar precio",
+  "image": "images/iphone 17 pro (5).jpeg"
+},
+{
+  "id": "iphone17-pro-6",
+  "name": "Funda iPhone 17 Pro",
+  "model": "iPhone 17 Pro",
+  "price": "Consultar precio",
+  "image": "images/iphone 17 pro (6).jpeg"
+},
+{
+  "id": "iphone17-promax-1",
+  "name": "Funda iPhone 17 Pro Max",
+  "model": "iPhone 17 Pro Max",
+  "price": "Consultar precio",
+  "image": "images/iphone 17 pro max.jpeg"
+},
+{
+  "id": "iphone17-promax-2",
+  "name": "Funda iPhone 17 Pro Max",
+  "model": "iPhone 17 Pro Max",
+  "price": "Consultar precio",
+  "image": "images/iphone 17 pro max (2).jpeg"
+}
 ];
